6e504edb64336014339594b269e31db0374ce9a2
Abed Hasan <abedhasan79@gmail.com>
2022-09-19 22:15:41 -0400

    added question on js file

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,34 @@
 let startBtn = document.querySelector(".startBtn");
 let timerCountDown = document.querySelector(".timer");
 
+let abed = document.querySelector(".mCqs");
 
+var myQuestions = [
+	{
+		question: "What is 10/2?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
+
+
+abed.innerHTML = Object.values(myQuestions[0].answers).map((size) => `<section>
+<input type="radio" name="size" value="${size}" id="${size}">
+ <label for="${size}">${size}</label>
+</section>`).join(' ');
 
 function startQuiz(){
     setTime();
